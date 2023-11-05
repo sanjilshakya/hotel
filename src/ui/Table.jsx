06576ed5341b593374226f0e3af6ba839b463b54
Sanjil <sanzil99@gmail.com>
2023-11-05 09:30:31 -0500

    table made reusable using compound component pattern

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import { createContext, useContext } from "react";
 
 const StyledTable = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -58,3 +60,59 @@ const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
+
+const TableContext = createContext();
+
+export default function Table({ columns, children }) {
+  return (
+    <TableContext.Provider value={{ columns }}>
+      <StyledTable role="table">{children}</StyledTable>
+    </TableContext.Provider>
+  );
+}
+
+function Header({ children }) {
+  const { columns } = useContext(TableContext);
+  return (
+    <StyledHeader role="row" columns={columns}>
+      {children}
+    </StyledHeader>
+  );
+}
+
+function Row({ children }) {
+  const { columns } = useContext(TableContext);
+  return (
+    <StyledRow role="row" columns={columns}>
+      {children}
+    </StyledRow>
+  );
+}
+
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
+
+Table.Header = Header;
+Table.Row = Row;
+Table.Body = Body;
+Table.Footer = Footer;
+
+Header.propTypes = {
+  children: PropTypes.any,
+};
+
+Row.propTypes = {
+  children: PropTypes.any,
+};
+
+Body.propTypes = {
+  data: PropTypes.array,
+  render: PropTypes.func,
+};
+
+Table.propTypes = {
+  columns: PropTypes.string,
+  children: PropTypes.any,
+};
