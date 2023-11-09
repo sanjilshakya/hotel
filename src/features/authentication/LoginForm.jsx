b86f1340dde3cd2ae7412465b3679af6a2e19988
Sanjil <sanzil99@gmail.com>
2023-11-09 17:24:17 -0500

    removing the initial state

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("sanzil99@gmail.com");
-  const [password, setPassword] = useState("P@ssw0rd");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { isLoggingIn, login } = useLogin();
 
